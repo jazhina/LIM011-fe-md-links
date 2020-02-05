7d94bf730f95512c6efea5ebd3f710ad08399581
jazhina <jazmin12977@gmail.com>
2020-02-05 12:17:14 -0500

    Verificar si es archivo y si es MD

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const fs = require('fs');
+const pathNamrotee = process.argv[2];
 
 const verificar = (ruta) => {
   if (path.isAbsolute(ruta) === true) {
@@ -6,7 +8,26 @@ const verificar = (ruta) => {
   }
   return path.resolve(ruta);
 };
+const archivo = (ruta) => {
+  if (fs.statSync(ruta).isFile() === true) {
+    return true;
+  }
+  return false;
+};
 
+const MD = route =>{
+  if(path.extname(route) === '.md') {
+    return true;
+  }
+};
+
+const identifyFile = (route) => {
+  const leer = fs.lstatSync(route);
+  const archivo = leer.isFile();
+  return archivo;
+};
+console.log(archivo('./mds/example/read.md'));
+console.log(MD('./mds/example/read.md'));
 console.log(verificar('./mds/example/read.md'));
 
 module.exports = {
